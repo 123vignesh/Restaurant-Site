57a3a98f6a4279a35aa0592fb82b71f2d6f75b4e
FIRST_NAME LAST_NAME <vigneshmsmg2000@gmail.com>
2020-10-26 10:51:05 +0530

    Done with Assighnment01

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,33 +1,69 @@
 import React, { Component } from "react";
 
-import {
-  Card,
-  CardBody,
-  CardText,
-  CardTitle,
-  CardImg,
-  CardImgOverlay,
-} from "reactstrap";
-
-import { DISHES } from "../shared/dishes";
+import { Card, CardBody, CardText, CardTitle, CardImg } from "reactstrap";
 
 export default class DishDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      
-    };
+    this.state = {};
   }
 
+  DateFormat = (date) => {
+    var k = date.split("T");
+    let monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var t = k[0].split("-")[1];
+    var t1 = k[0].split("-")[0];
+    var t2 = k[0].split("-")[2];
+    if (t < 10) {
+      var Mon = t[1];
+      var fin = monthNames[Mon - 1];
+
+      var g = fin + " " + t2 + "," + t1;
+      return g;
+    } else {
+      var Mon1 = t;
+      var fin1 = monthNames[Mon1 - 1];
+      var y = fin1 + " " + t2 + "," + t1;
+      return y;
+    }
+  };
+  go = (com) => {
+    if (com !== null) {
+      var result = com.map((co) => {
+        return (
+          <ul className="list-unstyled">
+            <li>{co.comment}</li>
+            <br />
+            <li>
+              -- {co.author}, {this.DateFormat(co.date)}
+            </li>
+          </ul>
+        );
+      });
+      return result;
+    } else {
+      <div></div>;
+    }
+  };
   render() {
     return (
-      <Card>
-        <CardImg top src={this.props.src} alt={this.props.name} />
-        <CardBody>
-          <CardTitle>{this.props.name}</CardTitle>
-          <CardText>{this.props.description}</CardText>
-        </CardBody>
-      </Card>
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <Card key={this.props.id}>
+            <CardImg top src={this.props.src} alt={this.props.name} />
+            <CardBody>
+              <CardTitle>{this.props.name}</CardTitle>
+              <CardText>{this.props.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <header>
+            <h4>Comments</h4>
+          </header>
+          {this.go(this.props.comments)}
+        </div>
+      </div>
     );
   }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -33,59 +33,16 @@ export default class Menu extends Component {
           src={dish.image}
           name={dish.name}
           description={dish.description}
+          comments={dish.comments}
+          id={dish.id}
         />
       );
     } else {
       return <div></div>;
     }
   };
-  DateFormat = (date) => {
-    var k = date.split("T");
-    let monthNames = [
-      "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec",
-    ];
-    var t = k[0].split("-")[1];
-    var t1 = k[0].split("-")[0];
-    var t2 = k[0].split("-")[2];
-    if (t < 10) {
-      var Mon = t[1];
-      var fin = monthNames[Mon - 1];
-
-      var g = fin + " " + t2 + "," + t1;
-      return g;
-    } else {
-      var Mon1 = t;
-      var fin1 = monthNames[Mon1 - 1];
-      var y = fin1 + " " + t2 + "," + t1;
-      return y;
-    }
-  };
   
-  renderComments = (com) => {
-    if (com !== null) {
-      var result = com.map((co) => {
-        return (
-          <ul className="list-unstyled">
-            <li>{co.comment}</li>
-            <br />
-            <li>
-              --{co.author}, {this.DateFormat(co.date)}
-            </li>
-          </ul>
-        );
-      });
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <header>
-            <h4>Comments</h4>
-          </header>
-          {result}
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  };
+  
 
   render() {
     const menu = this.props.dishes.map((dish) => {
@@ -105,13 +62,8 @@ export default class Menu extends Component {
         <div className="container">
           <div className="row">{menu}</div>
 
-          <div className="row">
-            <div className="col-12 col-md-5 m-1">
               {this.renderDish(this.state.selectedDish)}
-            </div>
-
-            {this.renderComments(this.state.comments)}
-          </div>
+           
         </div>
       </div>
     );
