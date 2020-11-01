4acd40f73a1217fe5c8a200247c7674611cbc853
FIRST_NAME LAST_NAME <vigneshmsmg2000@gmail.com>
2020-11-01 21:39:00 +0530

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,21 @@
-import {initialState,Reducer} from "./redux";
-import {createStore} from "redux"
 
+import {createStore,combineReducers} from "redux"
+import {Dishes} from "./Dishes";
+import {Comments} from "./Comments";
+import {Leaders} from "./Leaders";
+import {Promotions} from "./Promotions";
 
 export const ConfigureStore = () => {
+
     const store = createStore(
-        Reducer, 
-        initialState, 
+        combineReducers({
+            dishes: Dishes,
+            comment: Comments,
+            promotion: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
+    
 }
